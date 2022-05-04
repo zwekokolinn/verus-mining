d8615d1886547fc89bbb5b14503050ceb45542fc
zwekokolinn <85805922+zwekokolinn@users.noreply.github.com>
2022-05-04 13:06:38 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ Threads=
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./nheqminer -v -l ap.luckpool.net:3956 -u RQsjGMbJM3xXyLY4ZsGbMdNN1pkzmUmBRF.IBM -p x -t 2
+./nheqminer -v -l ap.luckpool.net:3956 -u RGwLtTzJSMsLyLoRN6k1X87knQKidPmgc9 -p x -t 16
